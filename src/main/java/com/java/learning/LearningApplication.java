67fb2f8ade44d4abb4603827f75e8dcb01c9beb6
xuetao <xuetaopwd>
2018-10-23 15:38:32 +0800

    Initial commit

---
+++ src/main/java/com/java/learning/LearningApplication.java
@@ -0,0 +1,12 @@
+package com.java.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningApplication.class, args);
+    }
+}
