2713996b891edd7b838ccc27687f78ed3011f24e
xuetao <xuetaopwd>
2018-12-12 23:41:26 +0800

    add Executor

---
+++ src/main/java/com/learning/executor/ExecutorTask.java
@@ -0,0 +1,13 @@
+package com.learning.executor;
+
+/**
+ * @Author xuet
+ * @Description
+ * @Date 12/12/18
+ * @Version 1.0
+ */
+public class ExecutorTask {
+    public static void main(String[] args) {
+
+    }
+}
