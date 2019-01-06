a7f7fd520394752df247fb31d4c2f0d2a794c8d2
xuetao <gentle.taoxue@gmail.com>
2019-01-06 23:47:00 +0800

    insert use of new locks

---
+++ src/main/java/com/learning/lock/ReentrantLockDemo.java
@@ -0,0 +1,33 @@
+package com.learning.lock;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @Author xuet
+ * @Description ReentrantLock 实现synchronized的高级功能
+ * @Date 1/6/19
+ * @Version 1.0
+ */
+public class ReentrantLockDemo {
+    private Lock lock = new ReentrantLock();
+
+    public static void main(String[] args) {
+        ReentrantLockDemo reentrantLockDemo = new ReentrantLockDemo();
+        reentrantLockDemo.updatePersonScore(2);
+    }
+
+    public void updatePersonScore(int score) {
+        lock.lock();
+        try {
+            //此处进行更新操作，加锁是为了同一时间只能有一个请求来更改数据状态
+            System.out.println("update person score");
+        } catch (Exception e) {
+
+        } finally {
+            //操作完成切记释放锁的信息
+            lock.unlock();
+        }
+
+    }
+}
