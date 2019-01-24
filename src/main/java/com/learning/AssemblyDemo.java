8e6fa69c15c2aaad479bbd62e3e2d225584fda07
xuetao <gentle.taoxue@gmail.com>
2019-01-24 23:41:41 +0800

    Integer 自动拆装箱测试

--- src/main/java/com/learning/AssemblyDemo.java
+++ src/main/java/com/learning/AssemblyDemo.java
@@ -13,8 +13,11 @@ public class AssemblyDemo {
         Integer b = 2;
         Integer c = 3;
         Integer d = 3;
-        Integer e = 321;
-        Integer f = 321;
+        Integer e = 127;
+        Integer f = 127;
+
+        Integer g = 128;
+        Integer h = 128;
 
         int m = 1;
         int n = 2;
@@ -23,7 +26,14 @@ public class AssemblyDemo {
 
 
         System.out.println(a == m);
-        //Interger 之间对比只能使用equals
+        //e == f 在 -128 ~ 127 区间返回 true
+        System.out.println(e == f);
+        // 说明：var=?在-128至127之间的赋值，Integer对象是在IntegerCache.cache产生，会复用已有对象，
+        // 这个区间内的Integer值可以直接使用==进行判断，但是这个区间之外的所有数据，
+        // 都会在堆上产生，并不会复用已有对象，这是一个大坑，推荐使用equals方法进行判断。
+
+        //e == f 不在 -128 ~ 127 区间返回 false
+        System.out.println(g == h);
         System.out.println(c.equals(d));
         System.out.println(e.equals(f));
         System.out.println(c == (a + b));
