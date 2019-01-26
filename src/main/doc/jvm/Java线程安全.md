9fc97bbb447e40b19ddb19508fe1414907cd204f
xuetao <gentle.taoxue@gmail.com>
2019-01-27 02:16:39 +0800

    this 逃逸分析

---
+++ src/main/doc/jvm/Java线程安全.md
@@ -0,0 +1,94 @@
+---
+title: Java线程安全
+date: 2019-01-19
+comments: true 
+tags:
+- JVM
+categories:  
+- Java虚拟机
+---
+
+## Java操作共享数据
+
+`Java`操作共享数据分为五类。不可变，绝对线程安全，相对线程安全，线程兼容和线程对立。   
+
+### 不可变
+
+不可变的对象一定是线程安全的，只要一个不可变对象被正确的构造出来（没有发生this引用逃逸的情况)，那么外部访问的可见状态永远不会改变。   
+
+```java
+    /**
+     * 第一种：被final修饰的基本类型值不能改变
+     */
+    private final int number = 5;
+    private final String message = "hello";
+
+    /**
+     * 第二种：被final修饰的对象, 对象不能改变，但是对象中的变量值可以改变
+     */
+    private final Person person = new Person("zhangshan");
+
+    public static void main(String[] args) {
+
+        ThreadSafetyDemo threadSafetyDemo = new ThreadSafetyDemo();
+        System.out.println("不可变对象int：" + threadSafetyDemo.number);
+        System.out.println("不可变对象string：" + threadSafetyDemo.message);
+
+
+        System.out.println("不可变对象person：" + threadSafetyDemo.person);
+        threadSafetyDemo.person.setName("lishi");
+
+        //Cannot assign a value to final variable 'person'
+//        threadSafetyDemo.person = new Person();
+        System.out.println("不可变对象person：" + threadSafetyDemo.person);
+    }
+```
+
+这里分析一下this引用逃逸，当线程RunablTest1还未初始化demo对象时，线程RunablTest2尝试获取未初始化demo对象的变量，代码如下：   
+```java
+    private int i = 0;
+    static ThreadSafetyDemo demo;
+    public ThreadSafetyDemo() {
+        i = 1;
+        System.out.println("hello");
+    }
+    private static class RunablTest1 implements Runnable {
+        @Override
+        public void run() {
+            demo = new ThreadSafetyDemo();
+        }
+
+    }
+
+    private static class RunablTest2 implements Runnable {
+        @Override
+        public void run() {
+            try {
+                System.out.println(demo.i);
+            } catch (NullPointerException e) {
+                System.out.println("发生空指针错误：普通变量j未被初始化");
+            }
+            try {
+                System.out.println(demo.i);
+            } catch (NullPointerException e) {
+                System.out.println("发生空指针错误：final变量i未被初始化");
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        new Thread(new RunablTest2()).start();
+        new Thread(new RunablTest1()).start();
+    }
+    输出结果：
+    发生空指针错误：普通变量j未被初始化
+    发生空指针错误：final变量i未被初始化
+    hello
+    
+```
+
+
+
+
+
--- src/main/java/com/learning/thread/ThreadSafetyDemo.java
+++ src/main/java/com/learning/thread/ThreadSafetyDemo.java
@@ -9,31 +9,65 @@ import com.learning.entity.Person;
  * @Version 1.0
  */
 public class ThreadSafetyDemo {
-    /**
-     * 第一种：被final修饰的基本类型值不能改变
-     */
-    private final int number = 5;
-    private final String message = "hello";
+    //    /**
+//     * 第一种：被final修饰的基本类型值不能改变
+//     */
+//    private final int number = 5;
+//    private final String message = "hello";
+//
+//    /**
+//     * 第二种：被final修饰的对象, 对象不能改变，但是对象中的变量值可以改变
+//     */
+//    private final Person person = new Person("zhangshan");
+//
+//    public static void main(String[] args) {
+//
+//        ThreadSafetyDemo threadSafetyDemo = new ThreadSafetyDemo();
+//        System.out.println("不可变对象int：" + threadSafetyDemo.number);
+//        System.out.println("不可变对象string：" + threadSafetyDemo.message);
+//
+//
+//        System.out.println("不可变对象person：" + threadSafetyDemo.person);
+//        threadSafetyDemo.person.setName("lishi");
+//
+//        //Cannot assign a value to final variable 'person'
+////        threadSafetyDemo.person = new Person();
+//        System.out.println("不可变对象person：" + threadSafetyDemo.person);
+//    }
 
-    /**
-     * 第二种：被final修饰的对象, 对象不能改变，但是对象中的变量值可以改变
-     */
-    private final Person person = new Person("zhangshan");
+    private int i = 0;
+    static ThreadSafetyDemo demo;
+    public ThreadSafetyDemo() {
+        i = 1;
+        System.out.println("hello");
+    }
+    private static class RunablTest1 implements Runnable {
+        @Override
+        public void run() {
+            demo = new ThreadSafetyDemo();
+        }
 
-    public static void main(String[] args) {
-
-        ThreadSafetyDemo threadSafetyDemo = new ThreadSafetyDemo();
-        System.out.println("不可变对象int：" + threadSafetyDemo.number);
-        System.out.println("不可变对象string：" + threadSafetyDemo.message);
-
-
-        System.out.println("不可变对象person：" + threadSafetyDemo.person);
-        threadSafetyDemo.person.setName("lishi");
-
-        //Cannot assign a value to final variable 'person'
-//        threadSafetyDemo.person = new Person();
-        System.out.println("不可变对象person：" + threadSafetyDemo.person);
     }
 
+    private static class RunablTest2 implements Runnable {
+        @Override
+        public void run() {
+            try {
+                System.out.println(demo.i);
+            } catch (NullPointerException e) {
+                System.out.println("发生空指针错误：普通变量j未被初始化");
+            }
+            try {
+                System.out.println(demo.i);
+            } catch (NullPointerException e) {
+                System.out.println("发生空指针错误：final变量i未被初始化");
+            }
+        }
 
+    }
+
+    public static void main(String[] args) {
+        new Thread(new RunablTest2()).start();
+        new Thread(new RunablTest1()).start();
+    }
 }
